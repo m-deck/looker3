7e0cad572cf19b21176d50dd950b041d8f6658bc
abelcastilloavant <abel.castillo@avant.com>
2016-04-18 11:26:13 -0500

    testing response handlers

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-avant-looker3: Pull data from Looker with the fancy new 3.0 API 
+avant-looker3: interface with the Looker 3.0 API
 ===========
 
-Provided fortune smiles upon us, this package will actually work some day.
+Pull data from Looker with the fancy new 3.0 API
 
+As of now this package is not functional. Provided fortune smiles upon us, this package will actually work some day.
--- tests/testthat/test-run_inline_query.R
+++ tests/testthat/test-run_inline_query.R
@@ -6,8 +6,8 @@ describe("run_inline_query helpers properly called", {
     `avant-looker3:::logout_api_call` = function(...) NULL,
     `avant-looker3:::query_api_call` = function(...) NULL,
     `avant-looker3:::extract_login_token` = function(...) NULL,
-    `avant-looker3:::extract_data_from_inline_query` = function(...) NULL,
-    `avant-looker3:::handle_logout_response` = function(...) NULL, {
+    `avant-looker3:::handle_logout_response` = function(...) NULL,
+    `avant-looker3:::extract_query_result` = function(...) NULL, {
       expect_true(TRUE)  
   })
 })
