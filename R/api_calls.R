a2c8ad15a7d3c35bb7a119dc556962b78bd53a0f
abelcastilloavant <abel.castillo@avant.com>
2016-04-19 10:49:55 -0500

    added tokens to httr calls

--- R/api_calls.R
+++ R/api_calls.R
@@ -5,11 +5,11 @@ login_api_call <- function(base_url, client_id, client_secret) {
 
 logout_api_call <- function(base_url, token) {
   full_url <- paste0(base_url, "api/3.0/logout")
-  httr::DELETE(url = full_url)
+  httr::DELETE(url = full_url, add_headers(Authorization = paste0("token ", token)))
 }
 
 query_api_call <- function(base_url, token, model, view, fields, filters, limit = 10, streaming = TRUE){
   full_url <- paste0(base_url, "api/3.0/queries/run/json")
   request_body <- list(model = model, view = view, fields = fields, filters = filters, limit, streaming)
-  httr::POST(url = full_url, body = request_body, encode = "json")
+  httr::POST(url = full_url, add_headers(Authorization = paste0("token ", token)),  body = request_body, encode = "json")
 }
