6c0512e122ff59da3e897d9189717b981ac445b1
abelcastilloavant <abel.castillo@avant.com>
2016-04-20 08:38:41 -0500

    wrote response handlers based on an example

--- R/response_handlers.R
+++ R/response_handlers.R
@@ -1,14 +1,10 @@
 validate_response <- function(http_response) {
-
-  # TODO: validate that the response is a request 
-  # object with success status code.
-  httr:::content(http_response) %in% c("200", "202", "204")  
+  checkr::validate(httr:::status_code(http_response) %in% c("200", "202", "204")) 
   TRUE
 }
 
 extract_login_token <- function(login_response) {
   validate_response(login_response)
-
   httr:::content(login_response)$access_token
 }
 
@@ -17,17 +13,9 @@ handle_logout_response <- function(logout_response) {
   validate_response(logout_response)
 }
 
-
 extract_query_result <- function(query_response) {
-  validate_response(query_response)
-
-  # TODO: figure out how to extract the data
-  # output <- httr:::content(query_response)
-
-  # TODO: verify that the recombinator input is valid
-
-
-  # TODO: figure out how/if recombination is needed 
-  # recombinator::recombinator(output)
-
+  validate_respose(query_response)
+  data_from_query <- httr:::content(query_response)
+  recombinator::recombinator(data_from_query)
 }
+
