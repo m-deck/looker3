df095db1ff28d7daceb2ca1932a24930905557cd
abelcastilloavant <abel.castillo@avant.com>
2016-06-06 14:22:33 -0500

    version checkr

--- circle_installations.R
+++ circle_installations.R
@@ -1,6 +1,6 @@
 devtools::install_github("hadley/devtools");
 devtools::install_github("hadley/testthat@v0.10.0");
 devtools::install_github("klutometis/roxygen@v5.0.0");
-devtools::install_github("peterhurford/checkr");
+devtools::install_github("peterhurford/checkr@0.1.2");
 devtools::install_github("jimhester/covr")
 
